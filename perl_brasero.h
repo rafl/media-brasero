ad34ef5da319c2df99a907a66047ca260cf50b84
Florian Ragwitz <rafl@debian.org>
2009-07-20 06:24:42 +0200

    Add common header file.

---
+++ perl_brasero.h
@@ -0,0 +1,7 @@
+#include <gtk2perl.h>
+#include <brasero-drive.h>
+#include <brasero-drive-selection.h>
+#include <brasero-medium-selection.h>
+#include <brasero-volume.h>
+#include <perl_brasero_gtypes.h>
+#include "brasero-autogen.h"
